0799a3dfd67cbb7874098bb7cbc2be1a8fd564fc
Niklas Korz <korz.niklask@gmail.com>
2016-01-18 10:46:11 +0100

    Export submodules

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import {Template} from './template';
 
+export * from './template';
+export * from './parser';
+
 export function render(template, data) {
   return new Template(template).render(data);
 }
